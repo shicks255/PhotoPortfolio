de9452f869b4b1e972d282cd605c74f784b5e924
shicks <shicks255@yahoo.com>
2019-09-08 12:28:05 -0500

    adding the photo information to each photobox

--- src/components/PhotoBox.jsx
+++ src/components/PhotoBox.jsx
@@ -4,10 +4,9 @@ export default class PhotoBox extends React.Component
 {
     constructor(props)
     {
-        console.log(props);
         super(props);
         this.state = {
-            name: '/image/' + props.photo.fileName
+            pictureName: '/image/' + props.photo.fileName
         }
     }
 
@@ -16,11 +15,21 @@ export default class PhotoBox extends React.Component
         return(
             <div>
 
-                <img src={this.state.name}/>
+                <img src={this.state.pictureName}/>
+
+                <div>
+                    <p>Added {this.props.photo.addedOn}</p>
+                    <p>Description {this.props.photo.description}</p>
+                    <p>Exposure Time{this.props.photo.exposureTime}</p>
+                    <p>Fstop {this.props.photo.fnumber}</p>
+                    <p>Focal Length {this.props.photo.focalLength}</p>
+                    <p>ISO {this.props.photo.iso}</p>
+                    <p>Lens {this.props.photo.lensModel}</p>
+                    <p>Date taken {this.props.photo.taken}</p>
+                </div>
 
             </div>
         )
-
     }
 
 }
