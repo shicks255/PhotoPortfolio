b168caeea3d8b7813fe2bd07457f8ff68d3ae1e6
shicks <shicks255@yahoo.com>
2019-09-17 13:55:57 -0500

    more front end changes

--- src/components/ModalPhotoDetails.jsx
+++ src/components/ModalPhotoDetails.jsx
@@ -9,32 +9,27 @@ export default function ModalPhotoDetails(props)
         let dateAdded = new Date(props.modalPhoto.added);
         content =
             <div>
-                <div className={'row'}>
-                    <div className={'col-sm-6-auto col-md-4-auto col-lg-2-auto'}>
-                        {props.modalPhoto.description}
-                    </div>
-                    <div className={'col-sm-6-auto col-md-4-auto col-lg-2-auto'}>
-                        <b>Exposure:</b> {props.modalPhoto.exposureTime} s
-                    </div>
-                    <div className={'col-sm-6-auto col-md-4-auto col-lg-2-auto'}>
-                        <b>Fstop:</b> {props.modalPhoto.fnumber}
-                    </div>
-                    <div className={'col-sm-6 col-md-4 col-lg-2'}>
-                        <b>Focal length:</b> {props.modalPhoto.focalLength}
-                    </div>
-                    <div className={'col-sm-6 col-md-4 col-lg-2'}>
-                        <b>Iso:</b> {props.modalPhoto.iso}
-                    </div>
-                    <div className={'col-sm-6 col-md-4 col-lg-2'}>
-                        <b>Lens:</b> {props.modalPhoto.lensModel}
-                    </div>
-                    <div className={'col-sm-6 col-md-4 col-lg-2'}>
-                        <b>Added:</b> {dateAdded.toDateString()}
-                    </div>
-                    <div className={'col-sm-6 col-md-4 col-lg-2'}>
-                        <b>Date taken:</b> {dateTaken.toLocaleDateString()}
-                    </div>
-                </div>
+                <table>
+                    <tr>
+                        <td colSpan={2}>{props.modalPhoto.description}</td>
+                    </tr>
+                    <tr>
+                        <td><b>Exposure:</b> {props.modalPhoto.exposureTime}</td>
+                        <td><b>Fstop:</b> {props.modalPhoto.fnumber}</td>
+                    </tr>
+                    <tr>
+                        <td><b>Focal length:</b> {props.modalPhoto.focalLength}</td>
+                        <td><b>Iso:</b> {props.modalPhoto.iso}</td>
+                    </tr>
+                    <tr>
+                        <td colSpan={2}><b>Lens:</b> {props.modalPhoto.lensModel}</td>
+                    </tr>
+                    <tr>
+                        <td><b>Added:</b> {dateAdded.toDateString()}</td>
+                        <td><b>Date taken:</b> {dateTaken.toLocaleDateString()}</td>
+                    </tr>
+
+                </table>
             </div>
     }
     return content
--- src/components/PhotoBox.jsx
+++ src/components/PhotoBox.jsx
@@ -4,7 +4,6 @@ export default class PhotoBox extends React.Component
 {
     constructor(props)
     {
-        console.log(props.onClick);
         super(props);
         this.state = {
             pictureName: '/image/' + props.photo.fileName + '/thumbnail/'
@@ -17,9 +16,9 @@ export default class PhotoBox extends React.Component
             <div className={'col-sm-12 col-md-6 col-lg-4 bry-5 brx-lg-5 border'}>
                 <div className={'card'}>
                     <img className={'card-img-top'} alt={this.props.name} src={this.state.pictureName}/>
-                    <div onClick={(photo) => this.props.clickFunction(this.props.photo)} style={{cursor: 'pointer'}} className={'card-img-overlay'}>
+                    <div onClick={() => this.props.clickFunction(this.props.photo)} style={{cursor: 'pointer'}} className={'card-img-overlay'}>
                         <div>
-                            <h4><span className={'badge badge-secondary'}>{this.props.photo.description}</span></h4>
+                            <h4><span className={'badge badge-secondary'}>{this.props.photo.title}</span></h4>
                         </div>
                     </div>
                     <div className={'card-body'}>
