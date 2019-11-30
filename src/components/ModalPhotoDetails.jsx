dc08bac27706a25a51f7ab658fc6214c5501bc48
shicks <shicks255@yahoo.com>
2019-11-29 21:58:25 -0600

    adding tags

--- src/components/ModalPhotoDetails.jsx
+++ src/components/ModalPhotoDetails.jsx
@@ -5,39 +5,48 @@ export default function ModalPhotoDetails(props)
     let content = '';
 
     if (props.modalPhoto) {
+
+        let tagList = props.modalPhoto.tags.map((tag, index) =>
+            <span key={index}>{tag.name}</span>
+        );
+
+
+
         let dateTaken = new Date(props.modalPhoto.taken);
         let dateAdded = new Date(props.modalPhoto.addedOn);
         content =
             <div>
-                <table>
+                <table border={1}>
                     <tbody>
                     <tr>
-                        <i class={'fas fa-map-marker-alt'}></i>
-                        <td colSpan={2}>{props.modalPhoto.description}</td>
+                        <td colSpan={2}>
+                            <i className={'fas fa-map-marker-alt'}></i>
+                            {props.modalPhoto.description}
+                        </td>
                     </tr>
                     <tr>
                         <td>
-                            <i class={'fas fa-clock'}></i>
+                            <i className={'fas fa-clock'}></i>
                             <b>Exposure:</b> {props.modalPhoto.exposureTime}
                         </td>
                         <td>
-                            <i class={'fas fa-asterisk'}></i>
+                            <i className={'fas fa-asterisk'}></i>
                             <b>Fstop:</b> {props.modalPhoto.fnumber}
                         </td>
                     </tr>
                     <tr>
                         <td>
-                            <i class={'fas fa-ruler'}></i>
+                            <i className={'fas fa-ruler'}></i>
                             <b>Focal length:</b> {props.modalPhoto.focalLength}
                         </td>
                         <td>
-                            <i class={'fas fa-cloud-sun'}></i>
+                            <i className={'fas fa-cloud-sun'}></i>
                             <b>Iso:</b> {props.modalPhoto.iso}
                         </td>
                     </tr>
                     <tr>
                         <td colSpan={2}>
-                            <i class={'fas fa-camera'}></i>
+                            <i className={'fas fa-camera'}></i>
                             <b>Lens:</b> {props.modalPhoto.lensModel}
                         </td>
                     </tr>
@@ -53,6 +62,7 @@ export default function ModalPhotoDetails(props)
                     </tr>
                     </tbody>
                 </table>
+                {tagList}
             </div>
     }
     return content
