907b5f4aeb0f48709d60b0d8835c5bd9850ebb06
shicks <shicks255@yahoo.com>
2020-01-08 21:55:47 -0600

    adding more for styles.  good enough for now but still needs much work

--- src/components/ModalPhotoDetails.jsx
+++ src/components/ModalPhotoDetails.jsx
@@ -9,19 +9,22 @@ export default function ModalPhotoDetails(props)
     if (props.modalPhoto) {
 
         let mapBundle = '';
+        let mapColClassName = '';
 
         if (props.modalPhoto.lat && props.modalPhoto.long) {
             let lat = props.modalPhoto.lat.slice(0, props.modalPhoto.lat.length-1);
             let lng = props.modalPhoto.long.slice(0, props.modalPhoto.long.length-1);
+            mapColClassName = 'col';
 
             mapBundle =
                 <LoadScript
-                    id='script-loader'>
+                    id='script-loader'
+                    >
                     <GoogleMap
                         id='my-map'
                         mapContainerStyle={{
-                            height: "400px",
-                            width: "800px"
+                            height: "300px",
+                            width: "600px"
                         }}
                         zoom={15}
                         center={{
@@ -45,54 +48,55 @@ export default function ModalPhotoDetails(props)
         let dateAdded = new Date(props.modalPhoto.addedOn);
         content =
             <div className={'container-fluid'}>
-                <div className={'row'}>
-                    <div className={'col'}>
+                <div className={'row justify-content-center'}>
+                    <div className={mapColClassName}>
                         {mapBundle}
                     </div>
                     <div className={'col'}>
-
-                        <table border={1}>
+                        <table className={'table'}>
                             <tbody>
                             <tr>
                                 <td colSpan={2}>
                                     <i>
-                                        {props.modalPhoto.description}
+                                        <p className={'h3'}>
+                                            {props.modalPhoto.description}
+                                        </p>
                                     </i>
                                 </td>
                             </tr>
                             <tr>
                                 <td>
-                                    <i className={'fas fa-clock'}></i>
+                                    <i className={'fas fa-clock'}></i>&nbsp;
                                     <b>Exposure:</b> {props.modalPhoto.exposureTime}
                                 </td>
                                 <td>
-                                    <i className={'fas fa-asterisk'}></i>
+                                    <i className={'fas fa-asterisk'}></i>&nbsp;
                                     <b>Fstop:</b> {props.modalPhoto.fnumber}
                                 </td>
                             </tr>
                             <tr>
                                 <td>
-                                    <i className={'fas fa-ruler'}></i>
+                                    <i className={'fas fa-ruler'}></i>&nbsp;
                                     <b>Focal length:</b> {props.modalPhoto.focalLength}
                                 </td>
                                 <td>
-                                    <i className={'fas fa-cloud-sun'}></i>
+                                    <i className={'fas fa-cloud-sun'}></i>&nbsp;
                                     <b>Iso:</b> {props.modalPhoto.iso}
                                 </td>
                             </tr>
                             <tr>
                                 <td colSpan={2}>
-                                    <i className={'fas fa-camera'}></i>
+                                    <i className={'fas fa-camera'}></i>&nbsp;
                                     <b>Lens:</b> {props.modalPhoto.lensModel}
                                 </td>
                             </tr>
                             <tr>
                                 <td>
-                                    <i className={'fas fa-calendar-alt'}></i>
+                                    <i className={'fas fa-calendar-alt'}></i>&nbsp;
                                     <b>Added:</b> {dateAdded.toDateString()} {dateAdded.toLocaleTimeString()}
                                 </td>
                                 <td>
-                                    <i className={'fas fa-calendar-alt'}></i>
+                                    <i className={'fas fa-calendar-alt'}></i>&nbsp;
                                     <b>Date taken:</b> {dateTaken.toDateString()} {dateTaken.toLocaleTimeString()}
                                 </td>
                             </tr>
