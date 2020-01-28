2f979d52ac2650ff385d5b7e5d2fe4b588ce92d3
shicks <shicks255@yahoo.com>
2020-01-28 16:15:03 -0600

    getting the modal carousel working

--- src/App.js
+++ src/App.js
@@ -3,33 +3,39 @@ import Body from './components/Body';
 import '@fortawesome/fontawesome-free/css/fontawesome.min.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="jumbotron jumbotron-fluid">
+    return (
+        <div className="App">
+            <header className="jumbotron jumbotron-fluid">
 
-          <div className={'myModal'}>
-              <div className={'modalHolder'}>
-                  <img className={'modalImage'} src={'/image/frontenac.jpg'} style={{ maxWidth: "1080px" }}/>
-              </div>
-          </div>
+                <div className={"container"}>
+                    <h4>
+                        My Photography Site
+                    </h4>
+                </div>
+            </header>
 
-          <div className={"container"}>
-              <h4>
-                  My Photography Site
-              </h4>
+            <div className={'myModal'}>
+                <div className={'modalImageContainer'}>
+                    <div className={'modalLeft'}>
+                        <span className={"previous carousel-control-prev-icon"} aria-hidden="true"></span>
+                    </div>
+                    <img className={'modalImage'} src={'/image/frontenac.jpg'} style={{ maxWidth: "1250px" }}/>
+                    <div className={'modalRight'}>
+                        <span className={"next carousel-control-next-icon"} aria-hidden="true"></span>
+                    </div>
+                </div>
+            </div>
+
+            <div className={'container'}>
+                <Body />
+            </div>
+
+            <footer>
+                Footer
+            </footer>
+
         </div>
-      </header>
-
-      <div className={'container'}>
-        <Body />
-      </div>
-
-      <footer>
-        Footer
-      </footer>
-
-    </div>
-  );
+    );
 }
 
 export default App;
